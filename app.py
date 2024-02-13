d2193f4e7ff6b38510a89f08dcb283073d40a011
ChloeSAPage <chloe.s.a.page@Gmail.com>
2024-02-13 16:47:41 +0000

    added description

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_file, render_template
+import glob
 
 app = Flask(__name__)
 
@@ -7,11 +8,13 @@ app = Flask(__name__)
 def get_rat(rat_id):
 
     rat_pic_path = f"resources/rat{rat_id}.jpg"
+    
+    num_rat_pics = len(glob.glob("resources/*.jpg"))
 
     try:
         return send_file(rat_pic_path, mimetype="image/jpeg"), 200
     except FileNotFoundError:
-        return render_template("error.html"), 404
+        return render_template("error.html", amount=num_rat_pics), 404, {"Description": f"MAX Rat ID is 0{num_rat_pics}"}
 
 
 if __name__ == "__main__":
