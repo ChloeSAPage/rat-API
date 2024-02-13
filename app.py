7842af82def79594cf19be2b94d497896e89ae5a
ChloeSAPage <chloe.s.a.page@Gmail.com>
2024-02-13 13:50:15 +0000

    added error

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/get-rat/<rat_id>")
 def get_rat(rat_id):
-    
-    rat_pic_path = f"resources/rat{rat_id}.jpg" 
 
-    return send_file(rat_pic_path, mimetype="image/jpeg"), 200
+    rat_pic_path = f"resources/rat{rat_id}.jpg"
 
+    try:
+        return send_file(rat_pic_path, mimetype="image/jpeg"), 200
+    except FileNotFoundError:
+        return render_template("error.html"), 404
 
 
 if __name__ == "__main__":
